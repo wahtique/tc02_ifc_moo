34665411967c06e0e69c562549901ab2cd6bae7d
NaejDoree <jean.poree-de-ridder@utbm.fr>
2016-05-11 23:00:15 +0200

    Added file navigation and finding the next agent id to create (wheezy ver)

--- MOO/FonctionsFichiers/NewAgent.c
+++ MOO/FonctionsFichiers/NewAgent.c
@@ -8,9 +8,32 @@
 
 int F_NewAgent(char Nom[])
 {
+    char NomFichier[10];
+    DIR* rep = NULL;
+    struct dirent* fichierLu = NULL;
+    rep = opendir("./Agent");
+    if (rep == NULL){
+        mkdir("./Agent");
+    }
+    seekdir(rep,2);
+    while((fichierLu = readdir(rep)) != NULL){
+    //printf("Le fichier lu s'appelle '%s'", fichierLu->d_name);
+        strcpy(NomFichier,fichierLu->d_name);
+    }
+    NomFichier[strlen(NomFichier)-2]='\0';
+    int nbr = atoi(NomFichier)+1;
+    itoa(nbr,NomFichier,10);
+
+
+    closedir(rep);
+
     FILE* fichier = NULL;
-    mkdir("./Agent");
-    fichier = fopen("./Agent/1.g","w+");
+    char CheminFichier[10]="./Agent/";
+    strcat(CheminFichier,NomFichier);
+    strcat(CheminFichier,".g");
+    printf("Fichier : %s\n",CheminFichier);
+    fichier = fopen(CheminFichier,"w+");
+
     fprintf(fichier,"Nom : %s",Nom);
     fclose(fichier);
 }
