bae669c42a06f350c8ea5a398c804c1a9a2cf529
William <wil-94@hotmailil.fr>
2016-05-23 10:26:03 +0200

    debug de la fonction de creation de matrice des couts

--- MOO/creer_matrice_couts.c
+++ MOO/creer_matrice_couts.c
@@ -7,18 +7,18 @@
 //une fonction pour créer une matrice de cout pour une simulations
 //on considere l'agent median med comme preexistant il doit donc avoir une existance persistante
 //n est ici le nombre d'agents / missions
-void creer_matrice_couts(Agent agts[n], Mission m[n], int  n, Agent med, double matrice_couts[][])
+void creer_matrice_couts(Agent agts[], Mission m[], Agent med, double matrice_couts[][n], int n)
 {
-	int i, j, nb_crit;
-
+	int i, j;
 	for(i=0;i<n;i++)
 	{
-		for(j=0;j<n,j++)
+		for(j=0;j<n;j++)
 		{
-			matrice_couts[i][j].c = estimation(agts[i], m[j], med, m[j]);
+			matrice_couts[i][j].c = estimation(agts[i], m[j], med);
 			matrice_couts[i][j].barre = 0;
 			matrice_couts[i][j].encadre = 0;
 			matrice_couts[i][j].trait = 0;
+
 		}
-	}	
+	}
 }
--- MOO/creer_matrice_couts.h
+++ MOO/creer_matrice_couts.h
@@ -4,6 +4,6 @@
 #include "Agent.h"
 #include "Mission.h"
 
-creer_matrice_couts(Agent *, Mission *, int);
+creer_matrice_couts(Agent *, Mission *, Agent, double **, int);
 
 #endif // CREER_MATRICE_COUTS_H_INCLUDED
