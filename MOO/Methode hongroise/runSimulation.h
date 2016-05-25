be870a26e342c13cc1296568b87b7acfba3ca4fb
William <wil-94@hotmailil.fr>
2016-05-25 20:30:58 +0200

    correction du include

--- MOO/Methode hongroise/runSimulation.h
+++ MOO/Methode hongroise/runSimulation.h
@@ -5,6 +5,6 @@
 #include "../Mission.h"
 #include "../simulation.h"
 
-simulation runSimulation(int, simulation *, Agent *, Mission *, Agent);
+void runSimulation(int, simulation *, Agent *, Mission *, Agent);
 
 #endif // RUNSIMULATION_H_INCLUDED
