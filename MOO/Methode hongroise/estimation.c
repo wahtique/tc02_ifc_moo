48dc49fc3fee8b576c15d8b1a26b361473d4b94d
William <wil-94@hotmailil.fr>
2016-05-30 11:59:40 +0200

    noms de champs dans estimation.c

--- MOO/Methode hongroise/estimation.c
+++ MOO/Methode hongroise/estimation.c
@@ -12,7 +12,7 @@ double estimation(Agent agt, Mission mission, Agent agt_med)
 	double prix, perf_std = 0, perf_agt = 0, prix_std;
 	int i, j, n;
 
-	n = mission.nb_crit;
+	n = mission.a_DimPonderation;
 
 	//calcul de l'indice de performance médian
 	for(i=0;i<n;i++)
@@ -22,29 +22,29 @@ double estimation(Agent agt, Mission mission, Agent agt_med)
 		//l'id du critère qu'o nconsidère dans lê calcul du côté mission
 		//dans le tableau de scores de l'agent
 		j = 0;
-		while(mission.a_tPonderations[0][j] != agt_med.a_tScore[0][j])
+		while(mission.a_tPonderation[0][j] != agt_med.a_tScore[0][j])
 		{
 			j = j+1;
 
 		}
 
-		perf_std = perf_std + mission.a_tPonderations[1][i]*agt_med.a_tScore[1][j];
+		perf_std = perf_std + mission.a_tPonderation[1][i]*agt_med.a_tScore[1][j];
 	}
 
 
 	for(i=0;i<n;i++)
 	{
 		j = 0;
-		while(mission.a_tPonderations[0][j] != agt.a_tScore[0][j])
+		while(mission.a_tPonderation[0][j] != agt.a_tScore[0][j])
 		{
 			j = j+1;
 
 		}
-		perf_agt = perf_agt + mission.a_tPonderations[1][i]*agt.a_tScore[1][j];
+		perf_agt = perf_agt + mission.a_tPonderation[1][i]*agt.a_tScore[1][j];
 	}
 
 	//le cout standard = salaire * durée
-	prix_std = agt_med.a_Salaire * mission.a_duree;
+	prix_std = agt_med.a_Salaire * mission.a_Duree;
 	// le cout pour l'agent fourni en parametres : perf agt / perf mediane * cout standard
 	// on peut supprime rune variables pour rendre les choses plus opti mais cest moins clair
 	prix = (1.*perf_std/perf_agt)*prix_std;
